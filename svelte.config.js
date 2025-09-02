465521731d5e8f4280607731c613389d01b1cd34
JHAhmed <jamalhascientist@gmail.com>
2025-09-03 04:07:45 +0530

    feat: update build configuration to support static and auto adapters, add cross-env dependency

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,27 @@
-import adapter from '@sveltejs/adapter-vercel';
+// svelte.config.js
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapterAuto from '@sveltejs/adapter-auto';
+import adapterStatic from '@sveltejs/adapter-static';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// inlineStyleThreshold: Infinity,
-		adapter: adapter(),
-		alias: {
-			$components: 'src/lib/components',
-		},
-	},
 	preprocess: vitePreprocess(),
+	kit: {
+		// Use adapter-static if the `BUILD_STATIC` env var is set
+		adapter: process.env.BUILD_STATIC
+			? adapterStatic({
+					// Options for the static build
+					pages: 'build',
+					assets: 'build',
+					fallback: 'index.html', // Use 'index.html' as SPA fallback
+					precompress: false,
+					strict: true
+				})
+			: adapterAuto(),
+		alias: {
+			$components: 'src/lib/components'
+		}
+	}
 };
+
 export default config;
